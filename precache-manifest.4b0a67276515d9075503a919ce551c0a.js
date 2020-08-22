0fd8d706f7857393accc91cea450aca8779eeece
Rafael Assad <rafaassadeng@gmail.com>
2020-08-22 16:00:38 -0300

    Updates

---
+++ precache-manifest.4b0a67276515d9075503a919ce551c0a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f61f230899991a3163f0afaecf2c3730",
+    "url": "/starwars-planets/index.html"
+  },
+  {
+    "revision": "272d8376e5ff3750a9e0",
+    "url": "/starwars-planets/static/css/main.184489bd.chunk.css"
+  },
+  {
+    "revision": "1bddbf0cdd96f5d7fcae",
+    "url": "/starwars-planets/static/js/2.32e0fb36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/starwars-planets/static/js/2.32e0fb36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "272d8376e5ff3750a9e0",
+    "url": "/starwars-planets/static/js/main.f4b1e9bc.chunk.js"
+  },
+  {
+    "revision": "63af275750f9e805b0aa",
+    "url": "/starwars-planets/static/js/runtime-main.f516939e.js"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/starwars-planets/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "ffe919624b50d867324596a91a7f8b03",
+    "url": "/starwars-planets/static/media/star-wars-banner-art.ffe91962.jpg"
+  },
+  {
+    "revision": "6d9d6a5b5dd013ff61964151649769bd",
+    "url": "/starwars-planets/static/media/starwars-universe.6d9d6a5b.jpg"
+  }
+]);
